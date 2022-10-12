a9b3ddd8554ea37ba5129efbe8a0c8698f24d76b
Brian Wu <briankwu2@gmail.com>
2022-10-12 02:53:44 -0500

    Update crashMe.c

--- crashMe.c
+++ crashMe.c
@@ -1,22 +1,28 @@
+#include <stdio.h>
 #include <stdlib.h>
 
 // Function intended to try to overflow amount of allocated memory and crash the program.
-void crashFunction ()
+void crashFunction (int * count)
 {
-    long double * bigNumber;
-    bigNumber = (long double *) malloc(10000000); // Allocates a lot of memory
-    crashFunction();
+    printf("The amount of calls is: %d\n", *count);
+    *count += 1;
+    crashFunction(count);
 
 }
 
 int main(int argc, char const *argv[])
 {
-    crashFunction(); // Call the crashing function
+    int * countPtr;
+    int count = 0;
+    countPtr = &count;
+    crashFunction(countPtr); // Call the crashing function
     return 0;
 }
 
 /*
 Results are that you will very shortly get a segmentation fault which means that
-you have intruded into a memory area where the user is not allowed and therefore crashes the program
-
+you have intruded into a memory area where the user is not allowed and therefore crashes the program.
+What happens is that the stack gets overflowed by the amount of local variables within the function and
+therefore intrudes outside of the allowed stack area.
+For my local machine, around 261,000 is the mark of calls where it crashes.
 */
